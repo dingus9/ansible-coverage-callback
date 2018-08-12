8c87e1c5458bcc32dc520b4f406403522467cdc0
l.aminov <l.aminov@tinkoff.ru>
2018-08-12 22:30:21 +0500

    Setup-py tests

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 import re
-from setuptools import setup
+
+from os.path import expanduser
+from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 
 READMEFILE = "README.md"
@@ -17,6 +20,28 @@ def get_version():
         raise RuntimeError(
             "Unable to find version string in %s." % VERSIONFILE)
 
+class Installer(install):
+    def run(self):
+        # Regular install
+        install.run(self)
+
+        print 'Creating Ansible Callback symlink'
+
+        home = expanduser('~')
+        callback_plugins_dir = os.path.join(home, '.ansible/plugins/callback_plugins')
+        callback_plugins_dist_file = os.path.join(self.install_lib, 'callback_plugins', 'coverage.py')
+        callback_plugins_user_file = os.path.join(callback_plugins_dir, 'coverage.py')
+
+        if not os.path.exists(callback_plugins_dir):
+            os.makedirs(callback_plugins_dir)
+
+        if os.path.exists(callback_plugins_user_file):
+            os.remove(callback_plugins_user_file)
+
+        os.symlink(callback_plugins_dist_file, callback_plugins_user_file)
+
+        print 'Done'
+
 setup(
     name='ansible-coverage-callback',
     version=get_version(),
@@ -26,9 +51,11 @@ setup(
     author='Lev Aminov',
     author_email='l.aminov@tinkoff.ru',
     license='MIT',
-    packages=['callback_plugins'],
+    packages=find_packages(),
+    cmdclass={'install': Installer},
     install_requires=[
         'ansible>=2.4'
     ],
     include_package_data=True,
+    zip_safe=False
 )
